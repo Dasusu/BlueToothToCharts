365fa230853441a5cd032e02eae767b99ba55216
Roger Tseng <rogertseng1025@gmail.com>
2022-06-22 15:22:11 +0800

    Feat:new chart

---
+++ BlueToothToCharts/Store.swift
@@ -0,0 +1,20 @@
+//
+//  Store.swift
+//  BlueToothToCharts
+//
+//  Created by 曾子庭 on 2022/6/22.
+//
+
+import Foundation
+import Charts
+
+
+class CurrentData{
+    var x:Double = 0
+    var currentData = [ChartDataEntry]()
+    
+    func newData(){
+        
+    }
+    
+}
--- BlueToothToCharts/ViewController.swift
+++ BlueToothToCharts/ViewController.swift
@@ -11,24 +11,64 @@ import Charts
 
 class ViewController: UIViewController {
 
+
     var blueToothManager = HeartBlueTooth()
+//    var timer:Timer?
+    var x:Double = 0
+    var currentData:[ChartDataEntry] = []{
+        didSet{
+            setData()
+        }
+    }
+
+    
+    //MARK: - properties
+    private lazy var liveChart:LineChartView = {
+        let chart = LineChartView()
+        chart.backgroundColor = .systemRed
+        chart.animate(xAxisDuration: 1.5)
+        return chart
+    }()
     
     
-    
-    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         blueToothManager.dataArrayDelegate = self
-        // Do any additional setup after loading the view.
+//        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(fakeData), userInfo: nil, repeats: true)
+        configure()
+        setData()
     }
-
-
+    
+    func setData(){
+        let setData = LineChartDataSet(entries: currentData)
+        setData.drawCirclesEnabled = false
+        let data = LineChartData(dataSet: setData)
+        data.setDrawValues(false)
+        liveChart.data = data
+    }
+    
+    //MARK: - selectors
+//    @objc func fakeData(){
+//        setData()
+//    }
+    
+    //MARK: - configure
+    func configure(){
+        view.addSubview(liveChart)
+        
+        liveChart.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.centerY.equalToSuperview()
+            make.height.equalTo(liveChart.snp.width)
+        }
+    }
 }
 
 extension ViewController: DataArrayDelegate {
     func updateBreathArray(_ breath: Double) {
-        
+        x += 1
+        currentData.append(ChartDataEntry(x: x, y: breath))
     }
-    
-    
+
 }
